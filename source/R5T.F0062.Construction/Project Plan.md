7b581c55e25e74e93406bef43ba5167fbd5899ec
davidcoats <david@getsafetycone.com>
2022-10-12 13:12:20 -0700

    Initial commit.

---
+++ source/R5T.F0062.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0062.Construction
+Construction console project for the R5T.F0062 library.
---
+++ source/R5T.F0062/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0062
+{
+	/// <summary>
+	/// WinForms operators.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
